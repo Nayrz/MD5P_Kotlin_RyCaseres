e83087ea3429cb61f2f0b55c0cc9663bcb360f81
Ryan Caseres <nayrcaseres@yahoo.com>
2023-07-18 23:08:40 +0800

    KortlinProgram3

---
+++ src/main/kotlin/KotlinProgram3.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name: String = "John Wick"
+    println("Hello $name")
+    name = "Jack Ryan"
+    println("Hello $name")
+    //string interpolation($)
+    var age: Int = 36
+    var salary: Double = 1000.0
+    var skilllevel: Char = 'S'
+    var isActive: Boolean = true
+    println("His age is $age")
+    println("His salary is $salary")
+    println("His skill level is $skilllevel")
+    println("He is currently $isActive")
+}
